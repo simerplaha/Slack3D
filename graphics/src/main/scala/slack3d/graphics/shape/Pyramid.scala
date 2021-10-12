9d8e5c6ea12e12da293707848b11e26745a97e60
simerplaha <simer.j@gmail.com>
2021-10-12 16:32:58 +1100

    Pyramid

---
+++ graphics/src/main/scala/slack3d/graphics/shape/Pyramid.scala
@@ -0,0 +1,109 @@
+package slack3d.graphics.shape
+
+import slack3d.algebra.Vector3
+import slack3d.graphics.colour.Colour
+import slack3d.graphics.shape.line.LineOrRay
+
+object Pyramid {
+
+  def apply(colour: Colour = Colour.next()) = {
+
+    val point = Vector3(0d, 0.5d, 0.0d)
+
+    //base plane coordinates
+    val leftFront = Vector3(-0.5d, -0.5d, 0.5d)
+    val rightFront = Vector3(0.5d, -0.5d, 0.5d)
+    val rightBack = Vector3(0.5d, -0.5d, -0.5d)
+    val leftBack = Vector3(-0.5d, -0.5d, -0.5d)
+
+    val base =
+      Plane(
+        a = leftFront,
+        b = rightFront,
+        c = rightBack,
+        d = leftBack,
+        colour = colour,
+        flipNormal = false
+      )
+
+    val left: Triangle =
+      Triangle(
+        a = point,
+        b = leftBack,
+        c = leftFront,
+        colour = colour,
+        flipNormal = false
+      )
+
+    val front: Triangle =
+      Triangle(
+        a = point,
+        b = leftFront,
+        c = rightFront,
+        colour = colour,
+        flipNormal = false
+      )
+
+    val right: Triangle =
+      Triangle(
+        a = point,
+        b = rightFront,
+        c = rightBack,
+        colour = colour,
+        flipNormal = false
+      )
+
+    val back: Triangle =
+      Triangle(
+        a = point,
+        b = rightBack,
+        c = leftBack,
+        colour = colour,
+        flipNormal = false
+      )
+
+    new Pyramid(
+      head = Point(point),
+      left = left,
+      front = front,
+      right = right,
+      back = back,
+      base = base
+    )
+  }
+}
+
+case class Pyramid private(head: Point,
+                           left: Triangle,
+                           front: Triangle,
+                           right: Triangle,
+                           back: Triangle,
+                           base: Plane) extends Shape {
+
+
+  override type Self = Pyramid
+
+  def vectors() =
+    head.vector +: (left.vectors() ++ front.vectors() ++ right.vectors() ++ back.vectors() ++ base.vectors())
+
+  def height() =
+    (head.vector - base.center()).length()
+
+  override def map(f: Vector3[Double] => Vector3[Double]): Pyramid =
+    Pyramid(
+      head = head.map(f),
+      left = left.map(f),
+      front = front.map(f),
+      right = right.map(f),
+      back = back.map(f),
+      base = base.map(f)
+    )
+
+  override def buildMesh(mesh: Mesh[Point, LineOrRay, Triangle]): Unit = {
+    mesh.triangles += left
+    mesh.triangles += front
+    mesh.triangles += right
+    mesh.triangles += back
+    base buildMesh mesh
+  }
+}

5b83c7e28120b9f1297d6b55aa82c7af422617b9
simerplaha <simer.j@gmail.com>
2021-10-12 15:40:10 +1100

    Light

---
+++ graphics/src/main/scala/slack3d/graphics/light/Light.scala
@@ -0,0 +1,30 @@
+//package slack3d.graphics.light
+//
+//import slack3d.algebra.Vector3
+//import slack3d.graphics.colour.Colour
+//import slack3d.graphics.vertex.Vertex.Triangle
+//
+//object Light {
+//
+//  def apply(colour: Colour = Colour.White,
+//            position: Vector3[Double] = Vector3[Double](3d, 6.0d, 9.0d),
+//            ambientStrength: Double = 0.23d,
+//            specularStrength: Double = 0.5d)(implicit projector: LightProjector) =
+//    new Light(
+//      colour = colour,
+//      position = position,
+//      ambient = colour * ambientStrength,
+//      specularStrength = specularStrength
+//    )
+//}
+//
+//class Light private(val colour: Colour,
+//                    val position: Vector3[Double],
+//                    val ambient: Colour,
+//                    val specularStrength: Double)(implicit projector: LightProjector) {
+//
+//  def apply(camera: Option[Camera],
+//            triangle: Triangle): Array[Vertex.Triangle[Vector3[Double]]] =
+//    projector(camera, this, triangle)
+//
+//}
---
+++ graphics/src/main/scala/slack3d/graphics/light/LightProjector.scala
@@ -0,0 +1,48 @@
+//package slack3d.graphics.light
+//
+//import slack3d.algebra.Vector3
+//import slack3d.graphics.vertex.Vertex.Triangle
+//
+//trait LightProjector {
+//
+//  def apply(camera: Option[Camera],
+//            light: Light,
+//            triangle: Triangle): Array[Vertex.Triangle[Vector3[Double]]]
+//
+//}
+//
+//object LightProjector {
+//
+//  case object PhongLightProjector extends LightProjector {
+//
+//    def apply(camera: Option[Camera],
+//              light: Light,
+//              triangle: Triangle): Array[Vertex.Triangle[Vector3[Double]]] = {
+//      val normal = triangle.normal().normalise()
+//
+//      triangle.vectors() map {
+//        vector =>
+//          //diffuse
+//          val lightDirection = (light.position - vector).normalise()
+//          val diff = (normal dot lightDirection) max 0.0d
+//          val diffuse = light.colour * diff
+//
+//          camera match {
+//            case Some(camera) =>
+//              //specular
+//              val viewDir = (camera.position - vector).normalise()
+//              val reflectionDir = lightDirection.negate() reflectOpenGL normal
+//              val spec = Math.pow((viewDir dot reflectionDir) max 0.0d, 32)
+//              val specular = light.specularStrength * spec * light.colour
+//
+//              val result = (light.ambient + diffuse + specular) * triangle.colour
+//              Vertex.Triangle(vector, result)
+//
+//            case None =>
+//              val result = triangle.colour * (light.ambient + diffuse)
+//              Vertex.Triangle(vector, result)
+//          }
+//      }
+//    }
+//  }
+//}
